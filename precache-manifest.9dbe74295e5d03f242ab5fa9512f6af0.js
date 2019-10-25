59c1ba0ce3612193ffa6b6a45f90e49819de587d
Denze1l <dvornikovd.o.1998@gmail.com>
2019-10-25 14:30:31 +0300

    Updates

---
+++ precache-manifest.9dbe74295e5d03f242ab5fa9512f6af0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c94e6b87617f5418d137f897907eadf5",
+    "url": "/goit-react-hw-05-budget-planner/./index.html"
+  },
+  {
+    "revision": "69fadf21cbdb54e36406",
+    "url": "/goit-react-hw-05-budget-planner/./static/js/2.235aa5b1.chunk.js"
+  },
+  {
+    "revision": "55a90ab8438c419a6c00",
+    "url": "/goit-react-hw-05-budget-planner/./static/js/main.796a6967.chunk.js"
+  },
+  {
+    "revision": "9d46b4568231638354db",
+    "url": "/goit-react-hw-05-budget-planner/./static/js/runtime-main.db9cf849.js"
+  }
+]);

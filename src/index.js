d5f511cd9e366b67887f859279e8f870e487e50f
Denze1l <dvornikovd.o.1998@gmail.com>
2019-10-25 15:00:14 +0300

    fix styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import store from './components/redux/store';
+import './style.css';
 
 ReactDOM.render(
   <Provider store={store}>
